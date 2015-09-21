7968f359ae934633e152599c66e212effd1a4d9a
cameronmathies <cmathies@IT202.mr.preble.com>
2015-09-21 12:26:34 -0400

    CELLULAR AUTOMATA  11:27

--- New Unity Project 1/Assets/MapGenerator.cs
+++ New Unity Project 1/Assets/MapGenerator.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class MapGenerator : MonoBehaviour {
+	
+	public int width;
+	public int height;
 
-	// Use this for initialization
-	void Start () {
-	
+	public string seed;
+	public bool useRandomSeed;
+
+	[Range(0,100)]
+
+	public int randomFillPercent;
+
+	int[,] map;
+
+	       void Start()
+	       {
+		GenerateMap();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void GenerateMap()
+	{
+		map = new int[width,height];
+		RandomFillMap ();
 	}
-}
+
+	void RandomFillMap() {
+		if (useRandomSeed) {
+			seed = Time.time.ToString ();
+		}
+		System.Random psuedoRanondom = new System.Random (seed.GetHashCode ());
+
+		for (int x = 0; x < width; x ++) {
+
+			for (int y = 0; y < height; y ++) {
+				map[x,y] = (psuedoRanondom.Next(0,100) < randomFillPercent)? 1: 0;
+			}
+		}
+	}
+
+	void OnDrawGizmos()
+	{if (map != null)
+		{
+			for (int x = 0; x < width; x ++) {
+				
+				for (int y = 0; y < height; y ++) {
+					Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+					Vector3 pos = new Vector3(-width/2 + x + .5f, 0, -height/2 + y+.5f);
+					Gizmos.DrawCube(pos,Vector3.one);
+				}
+			}
+		}
+
+		}
+                 
+	}
